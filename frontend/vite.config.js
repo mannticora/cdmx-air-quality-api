eb4e654031ab107e259dd4acef1d462544ad2cc3
mannticora <ricardogarciafentanez@gmail.com>
2026-06-06 07:31:40 -0600

    Actualizacion del front

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/measurements': 'https://cdmx-air-quality-api-production.up.railway.app',
-      '/stats': 'https://cdmx-air-quality-api-production.up.railway.app',
-    }
+  build: {
+    outDir: 'dist',
   }
 })
